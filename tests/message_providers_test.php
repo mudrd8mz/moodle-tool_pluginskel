3468921609f7ca42be3481399fd31f1258f8e8d2
Alexandru Elisei <alexandru.elisei@gmail.com>
2016-06-25 11:25:27 +0300

    Add 'message_providers' feature and PHPUnit tests

---
+++ tests/message_providers_test.php
@@ -0,0 +1,90 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * File containing tests for the 'message_providers' feature.
+ *
+ * @package     tool_pluginskel
+ * @copyright   2016 Alexandru Elisei <alexandru.elisei@gmail.com>, David Mudrák <david@moodle.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use Monolog\Logger;
+use Monolog\Handler\NullHandler;
+use tool_pluginskel\local\util\manager;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once($CFG->libdir . '/setuplib.php');
+require_once($CFG->dirroot . '/' . $CFG->admin . '/tool/pluginskel/vendor/autoload.php');
+
+/**
+ * Message_providers test class.
+ *
+ * @package     tool_pluginskel
+ * @copyright   2016 Alexandru Elisei alexandru.elisei@gmail.com
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class tool_pluginskel_message_providers_testcase extends advanced_testcase {
+
+    /** @var string[] The test recipe. */
+    protected static $recipe = array(
+        'component' => 'messageproviderstest',
+        'name'      => 'Message_providers test',
+        'copyright' => '2016 Alexandru Elisei <alexandru.elisei@gmail.com>',
+        'features'  => array(
+            'all' => false
+        ),
+        'message_providers' => array(
+            array(
+                'name' => 'submission',
+                'capability' => 'mod/quiz:emailnotifysubmission'
+            ),
+        )
+    );
+
+    /**
+     * Test creating the db/messages.php file.
+     */
+    public function test_message_providers() {
+        $logger = new Logger('messageproviderstest');
+        $logger->pushHandler(new NullHandler());
+        $manager = manager::instance($logger);
+
+        $recipe = self::$recipe;
+        $manager->load_recipe($recipe);
+        $manager->make();
+
+        $files = $manager->get_files_content();
+        $this->assertArrayHasKey('db/messages.php', $files);
+        $messagesfile = $files['db/messages.php'];
+
+        // Verify the boilerplate.
+        $description = 'Plugin message providers are defined here.'; 
+        $this->assertContains($description, $messagesfile);
+
+        $moodleinternal = "defined('MOODLE_INTERNAL') || die();";
+        $this->assertContains($moodleinternal, $messagesfile);
+
+        // Verify if the message provider has been generated correctly.
+        $messageprovider = $recipe['message_providers'][0]['name'];
+        $this->assertContains("'".$messageprovider."' => array(", $messagesfile);
+
+        $capability = $recipe['message_providers'][0]['capability'];
+        $this->assertContains("'capability' => '".$capability."'", $messagesfile);
+    }
+}
